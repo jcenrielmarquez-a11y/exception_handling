fc7316107fee4db1b713598347279ad2c203d161
Jc Enriel Marquez <jcenrielmarquez@gmail.com>
2026-05-03 20:24:51 +0800

    Added the initial implementation of the SimpleCalculator class with a calculate method that supports addition and subtraction operations.

---
+++ simple_calculator.py
@@ -0,0 +1,7 @@
+class SimpleCalculator:
+    def calculate(self, num_1, num_2, operation):
+        if operation == "+":
+            return num_1 + num_2
+        elif operation == "-":
+            return num_1 - num_2
+      
